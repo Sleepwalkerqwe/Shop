84e6557f69c47980ed8ac4d5845c0c6c9aa68be1
Sleepwalkerqwe <anatik2005@gmail.com>
2025-02-23 12:00:00 +0100

    mongodb, mongoose | dotenv, cors, cookie parser, body parser

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
+const port = process.env.PORT || 3000;
+
+// middleware setup
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// routes
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DATABASE);
+    console.log("connected to DB successfuly");
+  } catch (err) {
+    console.log(`database error - ${err}`);
+  }
+}
 app.get("/", (req, res) => {
   console.log("get request - 200");
   res.send("Hello world");
 });
 
 app.listen(port, () => {
+  connectDB();
   console.log(`listening on port - ${port}`);
 });
