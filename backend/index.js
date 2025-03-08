ce807c324e330514dbe0d3b6d4a3b940367356f3
Sleepwalkerqwe <anatik2005@gmail.com>
2025-03-08 12:00:00 +0100

    prepared for deploying

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://shop-lovat-seven.vercel.app"],
     credentials: true,
   })
 );
