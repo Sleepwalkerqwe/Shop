1247af9c2d9eb30a985b6117a2b3829ebe173c6a
Sleepwalkerqwe <anatik2005@gmail.com>
2025-05-01 00:46:24 +0200

    new styles

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,4 +1,4 @@
 export const getBaseUrl = () => {
-  // return 'https://shop-backend-lime.vercel.app';
-  return 'http://localhost:5000';
+  return 'https://shop-backend-lime.vercel.app';
+  // return 'http://localhost:5000';
 };
